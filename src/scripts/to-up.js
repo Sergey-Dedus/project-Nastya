619d325e1009cef8fbf2a00439f922c08c202e66
Sergey-Dedus <68299141+Sergey-Dedus@users.noreply.github.com>
2021-01-18 17:16:28 +0200

    gallery of products is worked

--- src/scripts/to-up.js
+++ src/scripts/to-up.js
@@ -25,3 +25,21 @@
 //       slidesToScroll: 1
 //  });
 //     });
+
+// function showImg(event)
+// {
+//     event = event || window.event; // получаем объект события
+//     let iconImg = event.Target || event.srcElement; // определяем текущий объект
+//     if (iconImg.tagName == "IMG"){ // если тип элемента - изображение, то
+//         document.getElementById("#bigImg").src = iconImg.getAttribute('src');  // меняем значение src у элемента с id="bigImg" на src текущего объекта 
+//     }
+// } 
+
+let ic = document.querySelector(".select-img");
+ic.addEventListener('click', function(event) {
+     event = event || window.event; // получаем объект события
+    let iconImg = event.Target || event.srcElement; // определяем текущий объект
+    if (iconImg.tagName == "IMG"){ // если тип элемента - изображение, то
+        document.getElementById("bigImg").src = iconImg.getAttribute('src');  // меняем значение src у элемента с id="bigImg" на src текущего объекта 
+    }
+});
