98af86f29cbc565674e6d709afbc262f1b8d6af3
Sergey-Dedus <68299141+Sergey-Dedus@users.noreply.github.com>
2021-01-19 21:59:50 +0200

    save9

--- src/scripts/slick-slider.js
+++ src/scripts/slick-slider.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.slider-area__slider-img').slick({
         dots: true,
         arrows: false,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 2500,
         fade: true,
         fadeSpeed: 1000
