24c13d521b7ebd1f5332914967f4b347741c526e
Sergey-Dedus <68299141+Sergey-Dedus@users.noreply.github.com>
2021-02-03 12:05:31 +0200

    modal-2 is end

--- src/scripts/clothes.js
+++ src/scripts/clothes.js
@@ -1,29 +1,3 @@
-// $(".clothes-menu").hide(); // скрываем выпадающее меню
-// $(".clothes").hover(
-//   function(){
-//   $(".clothes-menu").stop().fadeToggle(300);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
-// );
-
-
-// // $(".clothes-menu").hide(); // скрываем выпадающее меню
-// // $(".five li:has('.submenu')").hover(
-// //   function(){
-// //   $(".five li ul").stop().fadeToggle(300);} /* отбираем элемент списка, который содержит элемент с классом .submenu и добавляем ему функцию при наведении, которая показывает и скрывает выпадающее меню */
-// // );
-
-
-// function hideMenu() {
-//   $('.menu__dropdown').slideUp(700);
-// }
-// function showMenu() {
-//   $('.menu__dropdown').slideDown(700);
-// }
-// $(document).ready(function() {
-//   $(".clothes").on("mouseenter", showMenu);
-//   $(".header-laptop__wrapper").on("mouseleave", hideMenu);
-// });
-
-
 function hideMenu() {
   $('.modal-2').slideUp(700);
 }
