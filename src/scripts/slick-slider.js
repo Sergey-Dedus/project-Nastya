7b01134a31dcd5fc4829d53c31e3d706958e44d3
Sergey-Dedus <68299141+Sergey-Dedus@users.noreply.github.com>
2021-02-06 09:38:05 +0200

    work slider

--- src/scripts/slick-slider.js
+++ src/scripts/slick-slider.js
@@ -3,11 +3,20 @@ $(document).ready(function () {
         dots: true,
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 2500,
+        autoplaySpeed: 3000,
         fade: true,
         fadeSpeed: 1000
     })
+
+
+    $(".slider-area__slider-img").on("beforeChange", function(event, slick, currentSlide){
+slick.$slides.find("h2, h3, .text-container__btn").removeClass("animate__fadeInRight")
 });
+$(".slider-area__slider-img").on("afterChange", function(event, slick, currentSlide){
+slick.$slides.eq(currentSlide).find("h2, h3, .text-container__btn").addClass("animate__fadeInRight")
+});
+});
+
 
 
 
